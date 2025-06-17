d588f0580a7028f6f0df3883c435614a2003bc41
GadisOmbak <137881572+GadisOmbak@users.noreply.github.com>
2025-06-17 14:39:00 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// supabase.js
+const SUPABASE_URL = 'https://xyzcompany.supabase.co'; // Ganti
+const SUPABASE_KEY = 'your-anon-key'; // Ganti
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
